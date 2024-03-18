3889a026a71d034c5351a05eb686d1f36811ba9d
EzequielCorreia <contato.ezequielcorreia@gmail.com>
2024-03-18 01:45:58 -0300

    init project

--- Web_CRUD_MVC/Web_CRUD_MVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Web_CRUD_MVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ Web_CRUD_MVC/Web_Crud_SqlServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_Crud_SqlServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
