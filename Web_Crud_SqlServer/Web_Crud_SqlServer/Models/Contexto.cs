f72e4bda64853143568c7c33c057c28680ed9313
EzequielCorreia <contato.ezequielcorreia@gmail.com>
2024-03-18 01:23:50 -0300

    COMMIT

---
+++ Web_Crud_SqlServer/Web_Crud_SqlServer/Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAMVC.Models;
+
+namespace Web_Crud_SqlServer.Models
+{
+
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+        public DbSet<Produto>Produto {get; set;}
+    }
+}
